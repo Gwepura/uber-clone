e90922add1255a5e5d0f1f36cd4c55b658b384c6
Gwepura <aketani32@gmail.com>
2024-12-11 07:02:05 +0200

    File & Folder Structure and Router Setup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,12 +1,10 @@
 import { Link, Stack } from "expo-router";
 import { StyleSheet, View, Text } from "react-native";
 
-
-
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
         <Text type="title">This screen doesn't exist.</Text>
         <Link href="/" style={styles.link}>
