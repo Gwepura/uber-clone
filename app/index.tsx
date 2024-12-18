6187974bc367816328919e753ecdc56c95121676
Gwepura <aketani32@gmail.com>
2024-12-18 10:51:32 +0200

    Setup Clerk Email Authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
 import { Redirect } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Home = () => {
-  return <Redirect href="/(auth)/welcome" />; 
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
+
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
